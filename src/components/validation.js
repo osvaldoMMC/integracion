3cfb3f43981e558a3271112b1d75178a2eee8fdb
OsvaldoMMC <osmamaco@outlook.com>
2023-05-12 23:33:15 -0500

    nuevo

--- src/components/validation.js
+++ src/components/validation.js
@@ -1,23 +1,32 @@
-const validation =(userData,errors,setErrors) =>{
+const validation = (userData, errors, setErrors) => {
     //username
-    if(!userData.username) setErrors({...errors,username: "Por favor completa este campo"});
-    else if(userData.username.length > 35) setErrors({...errors,username: "El nombre de usuario no puede tener mas de 35 caracteres"});
-    else if(!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(userData.username)){
-        setErrors({...errors, username:"Email invalido"});
-    }else{
-        setErrors({...errors, username:""});
+    if (!userData.username)
+      setErrors((prevErrors) => ({ ...prevErrors, username: "Por favor completa este campo" }));
+    else if (userData.username.length > 35)
+      setErrors((prevErrors) => ({
+        ...prevErrors,
+        username: "El nombre de usuario no puede tener más de 35 caracteres",
+      }));
+    else if (
+      !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(userData.username)
+    ) {
+      setErrors((prevErrors) => ({ ...prevErrors, username: "Email inválido" }));
+    } else {
+      setErrors((prevErrors) => ({ ...prevErrors, username: "" }));
     }
-
+  
     //password
-    if(userData.password.length<6 || userData.password.length>10){
-        setErrors({...errors, password:"la contraseña tiene que tener entre 6 y 10 caracteres"});
-    }else if(!/\d/.test(userData.password)){
-        setErrors({...errors, password:"Debe contener al menos un numero"});
+    if (userData.password.length < 6 || userData.password.length > 10) {
+      setErrors((prevErrors) => ({
+        ...prevErrors,
+        password: "La contraseña debe tener entre 6 y 10 caracteres",
+      }));
+    } else if (!/\d/.test(userData.password)) {
+      setErrors((prevErrors) => ({ ...prevErrors, password: "Debe contener al menos un número" }));
+    } else {
+      setErrors((prevErrors) => ({ ...prevErrors, password: "" }));
     }
-    else{
-        setErrors({...errors, password:""})
-    }
-};
-
-
-export default validation;
+  };
+  
+  export default validation;
+  
